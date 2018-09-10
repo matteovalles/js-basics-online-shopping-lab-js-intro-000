142037c4f4c9caf99b9cef1670e0516dbeff2e03
Matteo Valles <matteovalles@gmail.com>
2018-09-10 01:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,23 +15,17 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var contents = `In your cart, you have`
   if (cart.length === 0) {
-    return 'Your shopping cart is empty.'
-  } else if (cart.length === 1) {
-    contents = `${contents} ${cart[0].itemName} at $${cart[0].itemPrice}.`
-  } else if (cart.length === 2) {
-    contents = `${contents} ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-  } else if (cart.length > 2) {
-    for (let i = 0; i < cart.length; i++) {
-      if (i === cart.length - 1) {
-        contents = `${contents} and ${cart[i].itemName} at $${cart[i].itemPrice}.`
-      } else {
-        contents = `${contents} ${cart[i].itemName} at $${cart[i].itemPrice},`
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
       }
     }
+    return "In your cart, you have " + items.join(", ") + ".";
   }
-  return contents
 }
 
 function total() {
