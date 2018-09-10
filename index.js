6b403f311d51a1e1e219f9201bd9f213ad705d90
Matteo Valles <matteovalles@gmail.com>
2018-09-10 01:11:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
     contents = `${contents} ${cart[0].itemName} at $${cart[0].itemPrice}.`
   } else if (cart.length === 2) {
     contents = `${contents} ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-  } else {
+  } else (cart.length > 2){
     for (let i = 0; i < cart.length; i++) {
       if (i === cart.length - 1) {
         contents = `${contents} and ${cart[i].itemName} at $${cart[i].itemPrice}.`
